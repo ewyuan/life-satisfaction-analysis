602699a0c907cf6b349c9b7d2318444b104c5655
labib <labibc01@gmail.com>
2020-10-17 16:03:32 -0400

    merging

--- gss-cleaning-tutorial/gss_cleaning.R
+++ gss-cleaning-tutorial/gss_cleaning.R
@@ -36,7 +36,7 @@
 #### Workspace set-up ####
 library(janitor)
 library(tidyverse)
-setwd("/Users/labibchowdhury/coursework/STA304/ps2")
+setwd("/Users/labibchowdhury/coursework/STA304/sta304-ps2/gss-cleaning-tutorial")
 # Load the data dictionary and the raw data and correct the variable names
 raw_data <- read_csv("AAoTMnOv.csv")
 dict <- read_lines("gss_dict.txt", skip = 18) # skip is because of preamble content
