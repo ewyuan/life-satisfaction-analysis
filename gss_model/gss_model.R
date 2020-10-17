6501ddea85ba91af58f378b537aa31d8e36161c1
labib <labibc01@gmail.com>
2020-10-17 17:07:09 -0400

    updated model

--- gss_model/gss_model.R
+++ gss_model/gss_model.R
@@ -1,18 +1,33 @@
 #install.packages("survey")
+#install.packages("brms")
 library(janitor)
 library(tidyverse)
 library(survey)
+library(brms)
 
 data <- read_csv("gss.csv")
 
+# glm model
 N = 30530800
 n = length(data$feelings_life_binary)
 fpc.srs = rep(N, n)
 
 satisfied.design <- svydesign(id=~1, data=data, fpc=fpc.srs)
-satisfied.glm = svyglm(feelings_life_binary ~ as.factor(vis_minority) + hours_worked,
-                       satisfied.design, family="binomial")
-# family_income + hh_type + education + self_rated_health + self_rated_mental_health
+
+satisfied.glm <- svyglm(feelings_life_binary ~ as.factor(vis_minority) + as.factor(hours_worked) + as.factor(hh_type) + 
+                         as.factor(family_income) + as.factor(self_rated_health) + as.factor(self_rated_mental_health) + 
+                         as.factor(edudation),
+                        design=satisfied.design, family="binomial")
+
 satisfied.glm %>% 
   broom::tidy() %>% 
   knitr::kable()
+
+# bayesian model
+satisfied.brm <- brm(feelings_life_binary ~ as.factor(vis_minority) + as.factor(hours_worked) + as.factor(hh_type) + 
+                       as.factor(family_income) + as.factor(self_rated_health) + as.factor(self_rated_mental_health) + 
+                       as.factor(edudation),
+                     data=data,
+                     family=bernoulli(),
+                     seed=420)
+
